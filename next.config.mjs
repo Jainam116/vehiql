f03fc6cc4116359d33206324889c577c0368108c
Jainam116 <kharajaynam@gmail.com>
2025-04-13 21:43:03 +0530

    prep for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverComponentsHmrCache: false, // default to true
+    },
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: "jjrsnrxflbndscqbgmvw.supabase.co",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
